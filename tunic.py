78025def1325cee8895372de60b84e7dc6142d84
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-07-07 06:29:51 +0800

    Update: `Clash.Meta` dns-nameserver (#14)

--- tunic.py
+++ tunic.py
@@ -90,8 +90,8 @@ dns:
   default-nameserver:
     - https://223.5.5.5/dns-query
   nameserver:
-    - "quic://dns.adguard.com"
-    - "https://dns.cloudflare.com/dns-query#h3=true"
+    - "quic://dns.adguard-dns.com"
+    - "https://security.cloudflare-dns.com/dns-query#h3=true"
 rule-providers:
   direct:
     type: http
